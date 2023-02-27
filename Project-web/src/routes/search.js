828b8803e8c9199720ed438d750b4c23d62d4cad
haopd12 <hao.pd200200@sis.hust.edu.vn>
2023-02-27 18:51:00 +0700

    Initial Commit

---
+++ Project-web/src/routes/search.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const router = express.Router();
+const searchController = require('../app/controllers/SearchController');
+
+router.use('/', searchController.search);
+
+
+module.exports = router;
+
